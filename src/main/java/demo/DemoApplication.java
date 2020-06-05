e4bcfcb5ae089530b459fd636b370fa230d2529b
serginho6x <serginho_6x@hotmail.com>
2020-06-05 12:08:27 -0400

    db named to project

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -8,16 +8,68 @@ import org.springframework.stereotype.*;
 @Controller
 @SpringBootApplication
 public class DemoApplication 
-	{
+        {
+        ////////////////////////////////////////////////
     @RequestMapping("/")
     @ResponseBody
-    String home() 	
-    	{
-	    return "Hello World from srcmake!";
-    	}
+    String home()       
+        {
+        return "Hello World from Sergio!";
+        }
+        ////////////////////////////////////////////////
 
+
+        ////////////////////////////////////////////////
+        // /v1/fruits/add
+        @RequestMapping(value = "/v1/fruits/add", method=RequestMethod.POST)
+    @ResponseBody
+    String FruitsAdd(@RequestParam("fruitname") String fruitname, @RequestParam("quantity") Integer quantity)
+        {
+        // Start the response to return to the API user.
+        String response = "Called /v1/fruits/add.\n";
+        response += "You want to add " + Integer.toString(quantity) + " " + fruitname + "'s.";
+
+        // TODO: ADD CODE TO UPDATE THE DATABASE HERE.
+        
+        return response;
+        }
+        ////////////////////////////////////////////////
+
+
+        ////////////////////////////////////////////////
+        // /v1/fruits/subtract    
+        @RequestMapping(value = "/v1/fruits/subtract", method=RequestMethod.POST)
+    @ResponseBody
+    String FruitsSubtract()     
+        {
+        // TODO: ADD CODE TO ACCEPT PARAMETERS HERE.
+        // TODO: ADD CODE TO UPDATE THE DATABASE HERE.
+
+        return "Called fruits/subtract";
+        }
+        ////////////////////////////////////////////////
+
+
+        ////////////////////////////////////////////////
+        // /v1/fruits/check
+    @RequestMapping(value = "/v1/fruits/check", method=RequestMethod.GET)
+    @ResponseBody
+    String FruitsCheck()        
+        {
+        // TODO: ADD CODE TO ACCEPT PARAMETERS HERE.
+        // TODO: ADD CODE TO QUERY THE DATABASE HERE.
+
+        String response = "Called /v1/fruits/check.";
+        return response;
+        }
+        ////////////////////////////////////////////////
+
+
+        ////////////////////////////////////////////////
+        // MAIN
     public static void main(String[] args) 
-    	{
+        {
         SpringApplication.run(DemoApplication.class, args);
-    	}
-	}
+        }
+        ////////////////////////////////////////////////
+        }
